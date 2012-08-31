9d619b3de83a67561d78b2d9532f4138c03de745
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2012-08-31 22:36:03 +0900

    restructure directories

---
+++ lib/firegoby/session.rb
@@ -0,0 +1,36 @@
+require "launchy"
+
+class Firegoby::Session
+  def api_key
+    'c9ff9c9a49ec22663219d4848aa3e4a3'
+  end
+
+  def api_secret
+    '2d87172f8a72d07c'
+  end
+
+  def flickr
+    if @flickr.nil? then
+      @flickr = Flickr.new(token_file, api_key, api_secret)
+      auth_token
+    end
+    @flickr
+  end
+
+  def token_file
+    "#{ENV['HOME']}/.flickr_firegoby"
+  end
+
+  def auth_token
+    unless @flickr.auth.token
+      Launchy.open(@flickr.auth.login_link)
+      STDIN.gets
+      @flickr.auth.getToken
+      @flickr.auth.cache_token
+    end
+  end
+
+  def method_missing(method, *args)
+    flickr.send(method, *args)
+  end
+end
